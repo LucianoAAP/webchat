40ca641f4c2ac4f7b53b97d6fee5c0d3138ee5fb
Luciano AAP <lucianoalmeidaap@gmail.com>
2022-01-21 18:53:37 -0300

    removes trybe things

--- models/connection.js
+++ models/connection.js
@@ -6,11 +6,11 @@ let schema = null;
 async function connection() {
   if (schema) return Promise.resolve(schema);
   return MongoClient
-    .connect(process.env.DB_URL, {
+    .connect(process.env.DB_URL || 'mongodb://localhost:27017/webchat/', {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     })
-    .then((conn) => conn.db(process.env.DB_NAME))
+    .then((conn) => conn.db(process.env.DB_NAME || 'webchat'))
     .then((dbSchema) => {
       schema = dbSchema;
       return schema;
